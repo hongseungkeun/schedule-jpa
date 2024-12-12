3a6a1ef48ffa7417b703cf01660eea7dc7aa21d9
SeuneKeun <eiziong1@gmail.com>
2024-12-12 20:21:07 +0900

    feat: 프로젝트 생성

---
+++ src/test/java/com/sparta/scheduleJpa/ScheduleJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.scheduleJpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
