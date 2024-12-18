78a4cf173f667c3c19a7fccdbcad9aca51d5560b
SeuneKeun <eiziong1@gmail.com>
2024-12-19 00:39:06 +0900

    fix: scheduleId에 대한 댓글만이 아닌 모든 댓글이 다 나오는 문제 해결

--- src/main/java/com/sparta/scheduleJpa/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/sparta/scheduleJpa/domain/comment/repository/CommentRepository.java
@@ -4,6 +4,7 @@ import com.sparta.scheduleJpa.domain.comment.dto.response.CommentReadDetailRes;
 import com.sparta.scheduleJpa.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("SELECT new com.sparta.scheduleJpa.domain.comment.dto.response.CommentReadDetailRes" +
             "(c.id, c.content, c.createdAt, c.updatedAt, c.user.id, c.user.name) FROM Comment c " +
+            "WHERE c.schedule.id = :schedule_id " +
             "ORDER BY c.createdAt DESC")
-    List<CommentReadDetailRes> findAllByScheduleIdOrderByCreatedAtDesc(Long scheduleId);
+    List<CommentReadDetailRes> findAllByScheduleIdOrderByCreatedAtDesc(@Param("schedule_id") Long scheduleId);
 }
